282a6281a0a48e52c62f4495efc4df7a3a805f72
Yannick <20306025+yandeu@users.noreply.github.com>
2019-03-13 18:01:53 +0100

    Added credits.js

---
+++ webpack/credits.js
@@ -0,0 +1,18 @@
+/**
+ * PLEASE DO NOT REMOVE THIS NOTICE!
+ *
+ * @template        This Phaser game was built using phaser-project-template-es6 (https://github.com/yandeu/phaser-project-template-es6)
+ * @author          Yannick Deubel (https://github.com/yandeu)
+ * @copyright       2019 Yannick Deubel
+ * @license         {@link https://github.com/yandeu/phaser-project-template-es6/blob/master/LICENSE|MIT License}
+ */
+
+console.log(
+  '%c %c %c %c %c Built using phaser-project-template-es6 %c https://github.com/yandeu/phaser-project-template-es6',
+  'background: #ff0000',
+  'background: #ffff00',
+  'background: #00ff00',
+  'background: #00ffff',
+  'color: #fff; background: #000000;',
+  'background: none'
+)
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { InjectManifest } = require('workbox-webpack-plugin')
 
 module.exports = {
-  entry: './src/scripts/game.js',
+  entry: ['./src/scripts/game.js', './webpack/credits.js'],
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: '[name].bundle.js',
